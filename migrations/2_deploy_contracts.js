0e6fac0158450a373ccc8159dc1629b7d40b28cf
Suraya Shivji <surayashivji@gmail.com>
2018-01-04 18:37:05 -0800

    dependencies and front-end

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
-var EcommerceStore = artifacts.require("./EcommerceStore.sol");
+var ConvertLib = artifacts.require("./ConvertLib.sol");
+var MetaCoin = artifacts.require("./MetaCoin.sol");
 
 module.exports = function(deployer) {
- deployer.deploy(EcommerceStore);
+  deployer.deploy(ConvertLib);
+  deployer.link(ConvertLib, MetaCoin);
+  deployer.deploy(MetaCoin);
 };
--- truffle.js
+++ truffle.js
@@ -1,9 +1,12 @@
+// Allows us to use ES6 in our migrations and tests.
+require('babel-register')
+
 module.exports = {
   networks: {
-  development: {
-  host: "localhost",
-  port: 8545,
-  network_id: "*" // Match any network id
- }
+    development: {
+      host: '127.0.0.1',
+      port: 7545,
+      network_id: '*' // Match any network id
+    }
+  }
 }
-};
