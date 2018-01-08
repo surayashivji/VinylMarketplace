b0387570c5a2d26cd128414b173d4ece11e3a76c
Suraya Shivji <surayashivji@gmail.com>
2018-01-07 21:28:02 -0800

    bid / reveal bid logic

--- contracts/EcommerceStore.sol
+++ contracts/EcommerceStore.sol
@@ -30,6 +30,16 @@ contract EcommerceStore {
         uint totalBids;
         ProductStatus status;
         ProductCondition condition;
+        // which user bids and what product they bid on
+        // (address of bidder => (hashed bid string => bid struct))
+        mapping (address => mapping (bytes32 => Bid)) bids;
+    }
+
+    struct Bid {
+        address bidder;
+        uint productId;
+        uint value; // amount sent by bidder (not amount being bid, which is encrypted)
+        bool revealed;
     }
 
     function EcommerceStore() public {
@@ -50,8 +60,102 @@ contract EcommerceStore {
         productIdInStore[productIndex] = msg.sender;
     }
 
-    function getProduct(uint _id) view public returns (Product) {
+    function getProduct(uint _id) view public returns (uint, string, string, string, string, uint, uint, uint, ProductStatus, ProductCondition) {
         Product memory product = stores[productIdInStore[_id]][_id];
-        return product;
+        return (product.id, product.name, product.category, product.imageLink, 
+        product.descLink, product.auctionStartTime, product.auctionEndTime, 
+        product.startPrice, product.status, product.condition);
     }
+
+    // user can bid an amount on any given record
+    // in order to generate a sealed bid (can't see bid amount), user must call the keccak256 function
+    // *pass amount user is bidding and the secret string into the keccak256 hashing function*
+    // keccak256("10.5" + "secretstring").toString('hex') => c2f8990ee5acd17d421d22647f20834cc37e20d0ef11087e85774bccaf782737
+    // @param productId: product ID, bid: encrypted bid string
+    function bid(uint _productId, bytes32 _bid) payable public returns (bool) {
+        Product storage product = stores[productIdInStore[_productId]][_productId];
+        require (now >= product.auctionStartTime);
+        require (now <= product.auctionEndTime);
+        require (msg.value > product.startPrice);
+        require (product.bids[msg.sender][_bid].bidder == 0);
+        // msg.value is the amt sent (not amt bidded, which is encrypted)
+        product.bids[msg.sender][_bid] = Bid(msg.sender, _productId, msg.value, false);
+        product.totalBids += 1;
+        return true;
+    }
+
+    // revealing tells the contract how much the user bid (actual amount is initially encrypted)
+    // uses keccak256 algorithm to ccheck that bid amt + secret generates same hash as in bids mapping
+    function revealBid(uint _productId, string _amount, string _secret) public {
+        Product storage product = stores[productIdInStore[_productId]][_productId];
+        require (now > product.auctionEndTime);
+        bytes32 sealedBid = keccak256(_amount, _secret);
+
+        Bid memory bidInfo = product.bids[msg.sender][sealedBid];
+        require (bidInfo.bidder > 0);
+        require (bidInfo.revealed == false);
+
+        uint refund;
+        uint amount = stringToUint(_amount);
+
+        if (bidInfo.value < amount) {
+            // they didn't send enough money (sent less than their bid amount)
+            refund = bidInfo.value;
+        } else {
+            // if it's the first bid to reveal for product, set as highest bidder
+            if (address(product.highestBidder) == 0) {
+                product.highestBidder = msg.sender;
+                product.highestBid = amount;
+                product.secondHighestBid = product.startPrice;
+                refund = bidInfo.value - amount;
+            } else {
+                // bid is higher than current highest revealed bid
+                // record bidder and bid as highest, set second highest value to old bid amt
+                if (amount > product.highestBid) {
+                    product.secondHighestBid = product.highestBid;
+                    product.highestBidder.transfer(product.highestBid);
+                    product.highestBidder = msg.sender;
+                    product.highestBid = amount;
+                    refund = bidInfo.value - amount;
+                } else if (amount > product.secondHighestBid) {
+                    // reset second highest bid but refund item because they lost
+                    product.secondHighestBid = amount;
+                    refund = amount;
+                } else { // refund the item because they lost (bid is lower than highest bid)
+                    refund = amount;
+                }
+            }
+            if (refund > 0) {
+                msg.sender.transfer(refund);
+                product.bids[msg.sender][sealedBid].revealed = true;
+            }
+        }
+    }
+
+    // Helper Methods
+
+    // returns the highest bidder info for a product
+    function highestBidderInfo(uint _productId) view public returns (address, uint, uint) {
+        Product memory product = stores[productIdInStore[_productId]][_productId];
+        // return highest bid address, highest bid value, and second highest bid value
+        return (product.highestBidder, product.highestBid, product.secondHighestBid);
+    }
+
+    // returns total number of bids for a product
+    function totalBids(uint _productId) view public returns (uint) {
+        Product memory product = stores[productIdInStore[_productId]][_productId];
+        return product.totalBids;
+    }
+
+    function stringToUint(string str) pure private returns(uint) {
+        bytes memory b = bytes(str);
+        uint result = 0;
+        for (uint x = 0; x < b.length; x++) {
+            if (b[x] >= 48 && b[x] <= 57) {
+                result = result * 10 + (uint(b[x]) - 48);
+            }
+        }
+        return result;
+    }
+    
 }
